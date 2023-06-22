b411d493db4788d3e510708a74433e4da160085e
Ivan Pavlov <rayzer_s@mail.ru>
2023-06-22 08:48:58 +0300

    HW2

---
+++ HW2/Main.java
@@ -0,0 +1,21 @@
+package HW2;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        // Создаем односвязный список
+        LinkedList.Node head = linkedList.createNode();
+
+        // Выводим исходный список
+        System.out.println("Исходный список:");
+        linkedList.printLinkedList(head);
+
+        // Разворачиваем список
+        head = linkedList.Reverse(head);
+
+        // Выводим развернутый список
+        System.out.println("Развернутый список:");
+        linkedList.printLinkedList(head);
+    }
+}
